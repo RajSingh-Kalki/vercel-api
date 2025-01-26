ccf1757d0074c9acb31e59bfefc68c1e62def971
RajSingh-Kalki <23f1000422@ds.study.iitm.ac.in>
2025-01-26 17:46:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_cors import CORS
 import json
 
 app = Flask(__name__)
-CORS(app, resources={r"/api/*": {"origins": "*"}})  # Allow all origins for /api/*
+CORS(app, resources={r"/api/*": {"origins": "*"}})
 
 # Load marks data from q-vercel-python.json
 with open('q-vercel-python.json') as f:
